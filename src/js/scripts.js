29226cd3e5226f480fbc2f23434c344a8d856d98
Carlos Dotas <42626583+carlosdotas@users.noreply.github.com>
2021-03-19 14:05:23 -0300

    Inclucao de Imports

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,48 @@
+/**
+ * resultado aproximado de uma circunferência de diâmetro igual a 20.000
+ *
+ * @type {number}
+ */
+export const PI = Math.PI // 3.141592653589793
+
+/**
+ * recebe por parâmetro uma quantidade indefinida de números e efetua a soma dos
+ * mesmos.
+ *
+ * @param {number[]} params
+ * @returns {number} valor soma dos parâmetros
+ */
+export function soma (...params) {
+ return params.reduce((a, b) => a + b, 0)
+}
+
+
+///////////////////////////////////////////////////
+///////// Propriedades
+///////////////////////////////////////////////////
+export const servidor = {
+	url:'http://localhost/vendasOnline/',
+	versao: '1.0'
+}
+
 ///////////////////////////////////////////////////
 ///////// Metodos
 ///////////////////////////////////////////////////
+/*
 $( document ).ready(function(){
-var layout={
-		head:{},
-		body:{
-			header:{},
-			nav:{},
-			section:{
+	var layout={
+			head:{},
+			body:{
 				header:{},
-				article:{},
-				footer:{},
-			},
-			aside:{},
-			footer:{}
+				nav:{},
+				section:{
+					header:{},
+					article:{},
+					footer:{},
+				},
+				aside:{},
+				footer:{}
+			}
 		}
-	}
-})
+});
+*/
